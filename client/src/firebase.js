9d297393889cce7a35bae10e337ee7df5839ec34
sherwin-roger0 <sherwinroger02@gmail.com>
2021-05-04 07:05:06 -0400

    ChatAir

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,13 +2,7 @@ import firebase from "firebase/app"
 import "firebase/auth"
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDORDIwFU4n0uHSX_sYzM2bNUUlyp1HLoU",
-    authDomain: "react-auth-87289.firebaseapp.com",
-    projectId: "react-auth-87289",
-    storageBucket: "react-auth-87289.appspot.com",
-    messagingSenderId: "325601634008",
-    appId: "1:325601634008:web:c8d20db84ee118da15d9ad",
-    measurementId: "G-50SB8PYG41"
+
   };
   // Initialize Firebase
   const app=firebase.initializeApp(firebaseConfig);
